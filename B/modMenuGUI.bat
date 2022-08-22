e72cb09128ccf3a145f1b2178c7a50ebe3eb6781
gabriellantunes <106894238+gabriellantunes@users.noreply.github.com>
2022-08-21 23:58:14 -0300

    Update modMenuGUI.bat

--- B/modMenuGUI.bat
+++ B/modMenuGUI.bat
@@ -3,25 +3,29 @@ cls
 :menu
 cls
 color 0
-
-echo Data de abertura do Mod Menu GUI:  
+echo 3
+echo 2
+echo 1...
+echo Iniciado com sucesso !!
+echo Version 1.00.0.0  
 date /t
 
 echo Computador: %computername%        Usuario: %username%
                    
-echo                                           	 MOD MENU GUI
-echo                     ==========================================================================
-echo                    * 1. Esvaziar a Lixeira                                                   +
-echo                    * 2. Escanear Disco Local                                                 +
-echo                    * 3. Painel de Controle                                                   +
-echo                    * 4. Iniciar o Google Chrome(se tiver instalado na maquina)               +
-echo                    * 5. Iniciar o Microsoft Edge(se tiver instalado na maquina)              +
-echo                    * 6. Iniciar o Outlook(se tiver instalado na maquina)                     +
-echo                    * 7. Iniciar o Word(se tiver instalado na maquina)                        +
-echo                    * 8. Agendar um desligamento                                              +
-echo                    * 9. Iniciar o Brave(se tiver instalado na maquina)                       +
-echo                    * 0. Sair                                                                 +
-echo                     ==========================================================================
+echo                                           	        MOD MENU GUI
+echo                     =============================================================================
+echo                    * 1.  Esvaziar a Lixeira                                                      +
+echo                    * 2.  Escanear Disco Local(Executar o Mod Menu Gui como Administrador)        +
+echo                    * 3.  Painel de Controle                                                      +
+echo                    * 4.  Iniciar o Google Chrome(se tiver instalado na maquina)                  +
+echo                    * 5.  Iniciar o Microsoft Edge(se tiver instalado na maquina)                 +
+echo                    * 6.  Iniciar o Outlook(se tiver instalado na maquina)                        +
+echo                    * 7.  Iniciar o Word(se tiver instalado na maquina)                           +
+echo                    * 8.  Agendar um desligamento(Não funciona quando executa como Administrador)+
+echo                    * 9.  Iniciar o Brave(se tiver instalado na maquina)                          +
+echo                    * 10. Iniciar o Excel(se tiver instalado na maquina)                          +
+echo                    * 0.  Sair                                                                    +
+echo                     =============================================================================
 
 set /p opcao= Escolha uma opcao: 
 echo ------------------------------
@@ -34,6 +38,7 @@ if %opcao% equ 6 goto opcao6
 if %opcao% equ 7 goto opcao7
 if %opcao% equ 8 goto opcao8
 if %opcao% equ 9 goto opcao9
+if %opcao% equ 10 goto opcao10
 if %opcao% GEQ 0 goto opcao0
 
 :opcao1
@@ -85,17 +90,10 @@ start brave.exe
 pause
 go to menu
 
-:opcao8
-start sistema.bat
-exit
+:opcao10
+start EXCEL.exe
+goto menu
 
 :opcao0
 cls
 exit
-
-
-
-
-
-
-
