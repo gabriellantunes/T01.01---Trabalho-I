e78ce88a3445470326d7b7db41ce6dd05f0b2ceb
gabriellantunes <106894238+gabriellantunes@users.noreply.github.com>
2022-08-21 23:57:52 -0300

    Update mensagem.bat

--- B/mensagem.bat
+++ B/mensagem.bat
@@ -2,30 +2,16 @@
 cls
 :menu
 cls
-color 0
-echo 3
-echo 2
-echo 1...
-echo Iniciado com sucesso !!
-echo Version 1.00.0.0  
 date /t
 
-echo Computador: %computername%        Usuario: %username%
-                   
-echo                                           	        MOD MENU GUI
-echo                     =============================================================================
-echo                    * 1.  Esvaziar a Lixeira                                                      +
-echo                    * 2.  Escanear Disco Local(Executar o Mod Menu Gui como Administrador)        +
-echo                    * 3.  Painel de Controle                                                      +
-echo                    * 4.  Iniciar o Google Chrome(se tiver instalado na maquina)                  +
-echo                    * 5.  Iniciar o Microsoft Edge(se tiver instalado na maquina)                 +
-echo                    * 6.  Iniciar o Outlook(se tiver instalado na maquina)                        +
-echo                    * 7.  Iniciar o Word(se tiver instalado na maquina)                           +
-echo                    * 8.  Agendar um desligamento(Não funciona quando executa como Administrador)+
-echo                    * 9.  Iniciar o Brave(se tiver instalado na maquina)                          +
-echo                    * 10. Iniciar o Excel(se tiver instalado na maquina)                          +
-echo                    * 0.  Sair                                                                    +
-echo                     =============================================================================
+echo                                =========================================
+echo                                * 1. Desligar a maquina em 30 minutos    +
+echo                                * 2. Desligar a maquina em 1 hora        +
+echo                                * 3. Desligar a maquina em 2 horas       +
+echo                                * 4. Desligar a maquina em 3 horas       +
+echo                                * 5. Desligar a maquina em 4 horas       +	
+echo                                * 6. Voltar para o menu principal        +
+echo                                =========================================
 
 set /p opcao= Escolha uma opcao: 
 echo ------------------------------
@@ -34,66 +20,34 @@ if %opcao% equ 2 goto opcao2
 if %opcao% equ 3 goto opcao3
 if %opcao% equ 4 goto opcao4
 if %opcao% equ 5 goto opcao5
-if %opcao% equ 6 goto opcao6
-if %opcao% equ 7 goto opcao7
-if %opcao% equ 8 goto opcao8
-if %opcao% equ 9 goto opcao9
-if %opcao% equ 10 goto opcao10
-if %opcao% GEQ 0 goto opcao0
+if %opcao% GEQ 6 goto opcao6
 
 :opcao1
-cls
-rd /S /Q c:\$Recycle.bin
-echo ===================================
-echo *      Lixeira Esvaziada          *
-echo ===================================
-pause
+shutdown -s -t 1800
 goto menu
+echo Desligamento agendado com sucesso.
 
 :opcao2
-cls
-echo ===================================
-echo *     Escaneamento de disco       *
-echo ===================================
-chkdsk c:
-pause
+shutdown -s -t 3600
 goto menu
+echo Desligamento agendado com sucesso.
 
 :opcao3
-cls
-control.exe
-pause
+shutdown -s -t 7200
 goto menu
+echo Desligamento agendado com sucesso.
 
 :opcao4
-start chrome.exe
+shutdown -s -t 10800
 goto menu
+echo Desligamento agendado com sucesso.
 
 :opcao5
-start msedge.exe
+shutdown -s -t 14400
 goto menu
+echo Desligamento agendado com sucesso.
 
 :opcao6
-start OUTLOOK.exe
-goto menu
-
-:opcao7
-start WINWORD.exe
-goto menu
-
-:opcao8
-start mensagem.bat
+start modMenuGui.bat
 exit
-
-:opcao9
-start brave.exe
-pause
-go to menu
-
-:opcao10
-start EXCEL.exe
 goto menu
-
-:opcao0
-cls
-exit
